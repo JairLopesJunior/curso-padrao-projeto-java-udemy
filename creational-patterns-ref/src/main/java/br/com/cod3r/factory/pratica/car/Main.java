c84b0b8064049d8c1791f315d2ad3d7ced5051cb
unknown <jair.lopes@fatec.sp.gov.br>
2023-03-08 22:18:34 -0300

    Abstract Factory #01

--- creational-patterns-ref/src/main/java/br/com/cod3r/factory/pratica/car/Main.java
+++ creational-patterns-ref/src/main/java/br/com/cod3r/factory/pratica/car/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car car = CarSimpleFactory.createCar("Gol");
+        Car car = CarSimpleFactory.createCar("Golf");
 
         System.out.println(car);
     }
